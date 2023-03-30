da49e508d09e3c12695711777df947d459b80e16
kritigirnar <kritigirnar@gmail.com>
2023-03-30 22:22:56 +0400

    Update Application .java

--- Application .java
+++ Application .java
@@ -63,7 +63,16 @@ public class Customer {
             }
         }
     
-    
+    JSONParser parser = new JSONParser();
+
+Object obj = parser.parse(new FileReader("simple.json"));
+
+JSONObject jsonObject = (JSONObject) obj;
+
+for(Iterator iterator = jsonObject.keySet().iterator(); iterator.hasNext();) {
+  String key = (String) iterator.next();
+  System.out.println(jsonObject.get(key));
+}
 
 
 }
