bcaf50b2b828f72d7771ab841fd56e7f49c413e8
kritigirnar <kritigirnar@gmail.com>
2023-03-30 22:02:56 +0400

    Update Application .java

--- Application .java
+++ Application .java
@@ -14,4 +14,32 @@ public class RestTemplateApplication {
     public RestTemplate getRestTemplate() {
         return new RestTemplate();
     }
+    
+    {
+    "customers": [
+        {
+            "customerId": "0434556574",
+            "paymentCode": "90501"
+        }
+    ]
+}
+You would need to classes to match the JSON structure:
+    Arrays are mentioned as -->[] IN. JSON
+        they are mapped with the help of list
+
+public class CustomersData {
+    private List<Customer> customers;
+
+    // Constructor, getter and setter
+}
+public class Customer {
+    private String customerId;
+    private String paymentCode;
+
+    // Constructor, getter and setter
+}
+    
+    
+
+
 }
