486cef85c694fcc07c173a2d31462129b20a1771
kritigirnar <kritigirnar@gmail.com>
2023-03-31 07:16:56 +0400

    Create Fldetai.java

---
+++ Fldetai.java
@@ -0,0 +1,13 @@
+import java.util.List;
+
+public class FlightDetails {
+    private List<Amenity> amenities;
+
+    public List<Amenity> getAmenities() {
+        return amenities;
+    }
+
+    public void setAmenities(List<Amenity> amenities) {
+        this.amenities = amenities;
+    }
+}
